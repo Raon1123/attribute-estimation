2f88c99a5be0dbd214d89d99692a4f8d3090e9d3
Yunpyo An <anyunpyo@unist.ac.kr>
2023-04-28 12:36:36 +0900

    update preproc

--- preprocessing/README.md
+++ preprocessing/README.md
@@ -2,14 +2,24 @@
 
 This directory preprocess each data
 
+## How to run preprocessing
+
+### Common argument
+- data_root: root directory of dataset
+
 ## Format
 
 metadata
-- img_root: root of image directory
-- label_list: mapping label number to label name string
+- `img_root`: root of image directory
+- `label_str`: mapping label number to label name string (length: L)
 
 each data
-- img_file: filename for image
-- pos_attr: positive attribute
-- neg_attr: negative attribute
-- unk_attr: unknown attribute
+- `train_img_file`: filename for train image
+- `val_img_file`: filename for validation image
+- `label`: label for multi-label classification, each image has (1,L) shape. If value is 1, this image has positive label, 0 negative label, and -1 unknown label. 
+
+## Example
+
+```bash
+python preprocess.py -D ~/dataset/RAP1 -S ~/dataset/attribute
+```
---
+++ preprocessing/preprocess.py
@@ -0,0 +1,72 @@
+import argparse
+import pickle
+import os
+
+import numpy as np
+from scipy.io import loadmat
+
+def preprocess_rap1(args):
+    data_len = 41585
+
+    data_root = args.data_dir
+    save_root = args.save_dir
+    idx = args.i
+
+    data = loadmat(os.path.join(data_root, 'RAP_annotation/RAP_annotation.mat'))
+    data = data['RAP_annotation']
+
+    img_root = os.path.join(data_root, 'RAP_dataset')
+    img_file = [data[0][0][5][i][0][0] for i in range(data_len)]
+
+    raw_attr_name = [data[0][0][3][i][0][0] for i in range(92)]
+    raw_label = data[0][0][1]
+    selected_attr_idx = np.array(range(51))
+
+    label = raw_label[:, selected_attr_idx].astype(np.float16)
+    attr_name = [raw_attr_name[i] for i in selected_attr_idx]
+
+    train_img_file = []
+    test_img_file = []
+
+    train = data[0][0][0][idx][0][0][0][0][0, :] - 1
+    test = data[0][0][0][idx][0][0][0][1][0, :] - 1
+
+    train_img_file.append([img_file[i] for i in train])
+    test_img_file.append([img_file[i] for i in test])
+
+    proc_dict = {
+        'img_root': img_root,
+        'label_str': attr_name,
+        'train_img_file': train_img_file,
+        'test_img_file': test_img_file,
+        'label': label,
+    }
+
+    with open(os.path.join(save_root, 'RAPv1.pkl'), 'wb') as f:
+        pickle.dump(proc_dict, f)
+
+
+def argparser():
+    parser = argparse.ArgumentParser(
+        prog='RAPv1 dataset preprocessing',
+        description='preprocess RAPv1 dataset for our framework'
+    )
+
+    parser.add_argument('-D', '--dataset', type=str, default='RAPv1', required=True)
+    parser.add_argument('-R', '--data_dir', type=str, required=True,
+                        help='root directory of raw dataset')
+    parser.add_argument('-S', '--save_dir', type=str, required=True,
+                        help='save preprocessed results')
+    parser.add_argument('-i', type=int, default=0, choices=range(5),
+                        help='index of RAPv1 dataset')
+    
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    # root_dir ~/dataset/RAP1
+    args = argparser()
+    if args.dataset == 'RAPv1':
+        preprocess_rap1(args)
+    else:
+        raise NotImplementedError
