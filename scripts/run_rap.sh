dcdff7db4058b0655cc80610a95b4b6e8f249800
Yunpyo An <anyunpyo@unist.ac.kr>
2023-05-07 14:18:51 +0900

    rm scripts

--- scripts/run_rap.sh
+++
@@ -1,2 +0,0 @@
-nohup python main.py --config configs/rap_config_beta.yml 1> outs/log_rap_beta.out 2> outs/log_rap_beta.err &
-nohup python main.py --config configs/rap_config.yml 1> outs/log_rap.out 2> outs/log_rap.err &
